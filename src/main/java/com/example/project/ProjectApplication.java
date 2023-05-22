204011baae2ef6178ebe10b10c5f9311153a3ba9
insun <kis8294@gmail.com>
2023-05-22 16:57:34 +0900

    스프링, 리액트 환경세팅

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectApplication {
-
+//깃허브 확인차 주석 달기
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
